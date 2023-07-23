5603218428349fe04f3e6eda76893a30bb0b6206
Cyprian Abala <arkwingscyprianabala@gmail.com>
2023-07-22 19:36:23 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num += 1;
+	}
+	printf("\n");
+	return (0);
+}
